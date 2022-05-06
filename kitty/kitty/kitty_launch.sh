85e544373d02e7ab93736c50b1223891ebbf1b6d
Ilias Rinis <iliasr@gmail.com>
2022-05-06 10:54:43 +0200

    [kitty] Launch standard kitty in fullscreen, quick terminal in normal mode

--- kitty/kitty/kitty_launch.sh
+++ kitty/kitty/kitty_launch.sh
@@ -1,1 +1,1 @@
-/Applications/kitty.app/Contents/MacOS/kitty -d=~ -o remember_window_size=no -o initial_window_width=140c -o initial_window_height=30c
+/Applications/kitty.app/Contents/MacOS/kitty -d=~ -o remember_window_size=no -o initial_window_width=140c -o initial_window_height=30c --start-as=normal
