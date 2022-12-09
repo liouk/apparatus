8e25e589b14fbbf22b136ce3a897e575e642b75f
Ilias Rinis <iliasr@gmail.com>
2022-12-09 23:46:25 +0100

    [nvim/telescope] Move picker config to the telescope setup to make it effective whenever the builtins are invoked

--- nvim/nvim/lua/plugin_config/telescope.lua
+++ nvim/nvim/lua/plugin_config/telescope.lua
@@ -13,27 +13,22 @@ require("telescope").setup({
       }
     },
   },
+  pickers = {
+    find_files = {
+      previewer = false,
+      find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+    },
+    oldfiles = { previewer = false },
+    buffers = { previewer = false },
+    live_grep = { only_sort_text = true },
+  },
 })
 
 local builtin = require("telescope.builtin")
-
-local function find_files_with_hidden()
-  builtin.find_files {
-    previewer = false,
-    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
-  }
-end
-
-local function live_grep_no_flepaths()
-  builtin.live_grep {
-    only_sort_text = true,
-  }
-end
-
-vim.keymap.set("n", "<C-p>", find_files_with_hidden)
+vim.keymap.set("n", "<C-p>", builtin.find_files)
 vim.keymap.set("n", "<leader>r", builtin.oldfiles)
 vim.keymap.set("n", "<leader>e", builtin.buffers)
-vim.keymap.set("n", "<leader>f", live_grep_no_flepaths)
+vim.keymap.set("n", "<leader>f", builtin.live_grep)
 
 -- use fzf native plugin
 require("telescope").load_extension("fzf")
