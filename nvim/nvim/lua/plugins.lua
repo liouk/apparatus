c08e04fe34c8783533f06280d865c3104038ca35
Ilias Rinis <iliasr@gmail.com>
2022-12-11 22:43:42 +0100

    [nvim] Move vim-expand-region plugin definition to nvim lua

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
   use 'APZelos/blamer.nvim'
   use 'airblade/vim-gitgutter'
   use 'tpope/vim-commentary'
+  use 'terryma/vim-expand-region'
 
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
