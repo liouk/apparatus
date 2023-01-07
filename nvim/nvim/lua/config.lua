734fb7f99ec3b5d6f7a331f5fef7bdd949d0834a
Ilias Rinis <iliasr@gmail.com>
2023-01-07 23:54:31 +0100

    [vim] Migrate vim configuration to neovim and delete it

--- nvim/nvim/lua/config.lua
+++ nvim/nvim/lua/config.lua
@@ -1,10 +1,24 @@
+vim.opt.ttimeoutlen = 0
+vim.wo.number = true
+
+-- identation
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
+-- colors
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "catppuccin-macchiato"
+vim.cmd([[
+highlight ExtraWhitespace ctermbg=darkblue guibg=#494d64
+autocmd BufEnter * exe ':2match ExtraWhitespace /\s\+$\| \+\ze\t\|\t\+\ze /'
+]])
 
 -- cursor: block everywhere, underline in insert
 vim.opt.guicursor = 'a:block,i:hor50'
