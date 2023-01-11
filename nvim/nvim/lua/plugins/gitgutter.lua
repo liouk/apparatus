1fecdda860baa9811d564cd1edaaddec539e9b63
Ilias Rinis <iliasr@gmail.com>
2023-01-11 23:01:38 +0100

    [nvim] Disallow gitgutter clobber in the sign column and decrease its priority in favor of lsp

--- nvim/nvim/lua/plugins/gitgutter.lua
+++ nvim/nvim/lua/plugins/gitgutter.lua
@@ -3,7 +3,8 @@ return {
   event = 'BufEnter',
 
   config = function()
-    vim.opt.signcolumn = 'yes'
+    vim.g.gitgutter_sign_priority = 0
+    vim.g.gitgutter_sign_allow_clobber = 0
     vim.cmd([[ autocmd BufWritePost * GitGutter ]])
   end,
 }
