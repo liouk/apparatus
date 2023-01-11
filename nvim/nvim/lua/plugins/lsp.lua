a7b2cccbf1a7bd401b22c0f0bf34190976bd5f25
Ilias Rinis <iliasr@gmail.com>
2023-01-11 23:02:07 +0100

    [nvim] Add useful lsp keymaps

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -45,9 +45,18 @@ return {
     local lspconfig = require('lspconfig')
     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lsp_attach = function(_, bufnr)
-      local opts = { buffer = bufnr, remap = false }
-      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+      local opts = { buffer = bufnr, noremap = true, silent = true }
       vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+      vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+      vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+      vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set('n', 'gn', function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set('x', 'ga', function() vim.lsp.buf.range_code_action() end, opts)
+      vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
+      vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
       vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
       vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
     end
