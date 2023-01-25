4096a2087f994369ff37f63100b78db79df0e5f2
Ilias Rinis <iliasr@gmail.com>
2023-01-25 16:49:23 +0100

    [nvim] Enable go.nvim keymappings only for current buffer

--- nvim/nvim/ftplugin/go.lua
+++ nvim/nvim/ftplugin/go.lua
@@ -2,5 +2,5 @@
 vim.opt.expandtab = false
 
 -- invoke gofmt+goimport from go.nvim instead of simply retabbing
-vim.keymap.set('n', '<leader>t', ':GoImport<CR>', { desc = 'Invoke gofmt+goimport [go.nvim]' })
-vim.keymap.set('n', '<leader>b', ':GoBuild<CR>', { desc = 'Invoke go build [go.nvim]' })
+vim.keymap.set('n', '<leader>t', ':GoImport<CR>', { desc = 'Invoke gofmt+goimport [go.nvim]', buffer = true })
+vim.keymap.set('n', '<leader>b', ':GoBuild<CR>', { desc = 'Invoke go build [go.nvim]', buffer = true })
