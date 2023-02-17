12fa54fc6904f33233e5e982e48ecd490a4c3c64
Ilias Rinis <iliasr@gmail.com>
2023-02-17 13:14:26 +0100

    [nvim/lualine] Hide encoding from status line

--- nvim/nvim/lua/plugins/lualine.lua
+++ nvim/nvim/lua/plugins/lualine.lua
@@ -15,7 +15,7 @@ return {
         lualine_a = {'mode'},
         lualine_b = {{'branch', icon = ''} , 'diff', 'diagnostics'},
         lualine_c = {{'filename', path = 1}},
-        lualine_x = {'encoding', 'filetype'},
+        lualine_x = {'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
       },
