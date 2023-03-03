85dcb77098e503996d133d1229a6ef5b8fc1ac90
Ilias Rinis <iliasr@gmail.com>
2023-03-03 13:20:26 +0100

    [install.sh/arch] Install yay

--- install.sh
+++ install.sh
@@ -141,6 +141,12 @@ function install_arch {
     kitty \
     unzip
 
+  pacman -S --needed git base-devel
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin
+  makepkg -si
+  cd ..
+  rm -rf yay-bin
   yay -S --answerclean N --answerdiff N --cleanafter --noremovemake --noconfirm --needed \
     navi ttf-jetbrains-mono-nerd
 }
