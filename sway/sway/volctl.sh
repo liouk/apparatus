e31eb4b80c296a2365f1f51bbbc8279a21ab44ee
Ilias Rinis <iliasr@gmail.com>
2023-03-17 15:56:57 +0100

    [sway] Do not increase volume beyond 100%

--- sway/sway/volctl.sh
+++ sway/sway/volctl.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-dp3="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink"
-dp2="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_4__sink"
-dp1="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink"
-t14s="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink"
-bose="bluez_output.2C_41_A1_4B_60_D1.1"
-sc2i2="alsa_output.usb-Focusrite_Scarlett_2i2_USB_Y89QGYR9C2E15D-00.analog-stereo"
-
 main () {
   all_sinks=$(pactl -f json list sinks|jq '.[] | "\(.state) \(.name)"')
-  active_sink=""
+  active_sink="@DEFAULT_SINK@"
   while read -r sink; do
     sink_status=(${sink//\"})
     sink_state="${sink_status[0]}"
@@ -21,30 +14,15 @@ main () {
       active_sink="$sink_name"
     fi
   done <<< "$all_sinks"
-  case "$active_sink" in
-    "$t14s"|"") active_sink="@DEFAULT_SINK@" active_sink_disp="t14s" ;;
-        "$dp1") active_sink_disp="DP-1" ;;
-        "$dp2") active_sink_disp="DP-2" ;;
-        "$dp3") active_sink_disp="DP-3" ;;
-       "$bose") active_sink_disp="bose" ;;
-      "$sc2i2") active_sink_disp="2i2"  ;;
-             *) active_sink_disp="???" ;;
-  esac
 
   audio_volume=$(pactl get-sink-volume "$active_sink" | cut -d'/' -f2 | head -1 | xargs)
-  audio_is_muted=$(pactl get-sink-mute "$active_sink" | cut -d' ' -f2)
-
   case "$1" in
-    get-vol)
-      echo -n "$audio_volume ($active_sink_disp)"
-      exit
-      ;;
-    get-mute)
-      echo -n "$audio_is_muted"
-      exit
-      ;;
     up)
-      pactl set-sink-volume "$active_sink" +5%
+      if (( ${audio_volume%\%}+5 > 100 )); then
+        pactl set-sink-volume "$active_sink" 100%
+      else
+        pactl set-sink-volume "$active_sink" +5%
+      fi
       pactl set-sink-mute "$active_sink" 0
       ;;
     down)
