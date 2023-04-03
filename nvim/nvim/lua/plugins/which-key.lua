cce1eb9cb5b0848014d4acd1100769163c43a56c
Ilias Rinis <iliasr@gmail.com>
2023-04-03 19:37:01 +0200

    [nvim] Add which-key.nvim

---
+++ nvim/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  'folke/which-key.nvim',
+  event = 'BufEnter',
+
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+    require('which-key').setup({})
+  end,
+}
