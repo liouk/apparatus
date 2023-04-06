9e25767983f0fcbd2a85abed8cb9abdb20ad532e
Ilias Rinis <iliasr@gmail.com>
2023-04-06 10:33:09 +0200

    [waybar] Add bt device battery when available; Show VPN tooltip and generalize script

--- waybar/waybar/vpn.sh
+++ waybar/waybar/vpn.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-vpn_conn_uuid="e69b124b-50a8-4458-8859-5e68c3f19460"
-
 vpn_status () {
-  nmcli connection show --active | grep --quiet "$vpn_conn_uuid"
+  nmcli connection show --active | grep --quiet "$VPN_CONN_UUID"
 }
 
 vpn_statusjson () {
   status=""
   vpn_status && status="connected" || status="disconnected"
-  echo "{\"class\": \"$status\"}"
+  echo "{\"class\": \"$status\", \"tooltip\": \"'$VPN_CONN_NAME' $status\"}"
 }
 
 vpn_toggle () {
@@ -17,16 +15,19 @@ vpn_toggle () {
 }
 
 vpn_up () {
-  nmcli connection up "$vpn_conn_uuid" --ask
+  nmcli connection up "$VPN_CONN_UUID" --ask
   pkill -RTMIN+1 waybar
 }
 
 vpn_down () {
-  nmcli connection down "$vpn_conn_uuid"
+  nmcli connection down "$VPN_CONN_UUID"
   pkill -RTMIN+1 waybar
 }
 
 main () {
+  VPN_CONN_UUID=$(nmcli -g NAME,UUID,TYPE connection show | grep vpn | head -1 | cut -d':' -f2)
+  VPN_CONN_NAME=$(nmcli -g NAME,UUID,TYPE connection show | grep vpn | head -1 | cut -d':' -f1)
+
   cmd="$1"
   case "$1" in
     status|statusjson|up|down|toggle) ;;
