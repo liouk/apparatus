2f08563e401d96e721e227e851cef4e6b0f1f6c8
Ilias Rinis <iliasr@gmail.com>
2023-04-11 23:09:46 +0300

    [nvim] Replace user/sessions.lua with my own jam-sessions.nvim plugin

---
+++ nvim/nvim/lua/plugins/jam-sessions.lua
@@ -0,0 +1,17 @@
+return {
+  'liouk/jam-sessions.nvim',
+  dependencies = { 'junegunn/fzf', 'junegunn/fzf.vim', 'nvim-lua/plenary.nvim' },
+  cmd = { 'SaveSession', 'LoadSession', 'DeleteSession' },
+  keys = { '<leader>ss', '<leader>sl',  '<leader>sd', '<leader>sq' },
+
+  config = function()
+    require('jam-sessions').setup({
+      dir = vim.g.sessions_path
+    })
+
+    vim.keymap.set('n', '<leader>ss', ':SaveSession<CR>')
+    vim.keymap.set('n', '<leader>sl', ':LoadSession<CR>')
+    vim.keymap.set('n', '<leader>sd', ':DeleteSession<CR>')
+    vim.keymap.set('n', '<leader>sq', ":exec 'SaveSession' <Bar> quit<CR>")
+  end,
+}
