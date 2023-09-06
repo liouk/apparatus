79fb2703496aaafe42474a83bce8d008cd2352e5
Ilias Rinis <iliasr@gmail.com>
2023-09-06 13:43:48 +0200

    [nvim/lualine] Add repo name to bottom bar

--- nvim/nvim/lua/plugins/lualine.lua
+++ nvim/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,32 @@
+local function repo()
+  local Job = require('plenary.job')
+  local git_remote
+  local exitcode
+  Job:new({
+    command = 'git',
+    args = {'config', '--get', 'remote.origin.url'},
+    on_exit = function(j, code)
+      git_remote = j:result()[1]
+      exitcode = code
+    end,
+  }):sync()
+
+  if exitcode > 0 then
+    return ""
+  end
+
+  local prefix = "git@github.com:"
+  local suffix = ".git"
+  local reponame
+  if string.sub(git_remote, 1, prefix:len()) == prefix then
+    reponame = string.sub(git_remote, prefix:len()+1, git_remote:len()-suffix:len())
+  else
+    return ""
+  end
+
+  return reponame
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -13,7 +42,7 @@ return {
       },
       sections = {
         lualine_a = {'mode'},
-        lualine_b = {{'branch', icon = ''} , 'diff', 'diagnostics'},
+        lualine_b = {{repo, icon = '󰊢'}, {'branch', icon = ''} , 'diff', 'diagnostics'},
         lualine_c = {{'filename', path = 1}},
         lualine_x = {'filetype'},
         lualine_y = {'progress'},
