4f420a5c3208687b39d04e2746efdf2936809e96
Ilias Rinis <iliasr@gmail.com>
2025-01-13 11:39:04 +0100

    [waybar] Show pause status

--- waybar/waybar/media.sh
+++ waybar/waybar/media.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 icon=""
+paused=
 for p in $(playerctl -l); do
-	if [[ "$(playerctl -s -p $p status)" == "Playing" ]]; then
-		if [[ "$p" == "spotify" ]]; then
-			icon=""
-		fi
-		echo "$icon $(playerctl -s -p $p metadata --format '{{ artist }} - {{ title }}')"
-		exit 0
-	fi
+	case "$(playerctl -s -p $p status)" in
+		"Playing")
+			if [[ "$p" == "spotify" ]]; then
+				icon=""
+			fi
+			echo "$icon $(playerctl -s -p $p metadata --format '{{ artist }} - {{ title }}')"
+			exit 0
+			;;
+		"Paused")
+			paused=1
+			;;
+	esac
 done
+
+if [[ -n "$paused" ]]; then
+	icon=""
+	echo "$icon $(playerctl -s -p $p metadata --format '{{ artist }} - {{ title }}')"
+	exit 0
+fi
