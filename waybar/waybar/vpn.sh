aa3b580e445651b0934d0a4d91beb5ab36f0b798
Ilias Rinis <iliasr@gmail.com>
2025-10-31 08:16:22 +0100

    [waybar] Use one icon for tailscale/redhat vpn

--- waybar/waybar/vpn.sh
+++ waybar/waybar/vpn.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+waybar_notify() {
+	pkill -RTMIN+1 waybar
+}
+
+tailscale_cmd() {
+	cmd="$1"
+	shift
+	case "$cmd" in
+		up) tailscale up "$@" && waybar_notify ;;
+		down) tailscale down && waybar_notify ;;
+		status) tailscale status ;;
+		*) echo "unknown command: '$cmd'" && exit 1 ;;
+	esac
+}
+
 tailscale_status() {
   local status_output
   status_output=$(tailscale status --json 2>/dev/null)
 
   if [[ $? -ne 0 ]] || [[ $(echo "$status_output" | jq -r '.BackendState') != "Running" ]]; then
-    echo "{\"class\": \"disconnected\", \"tooltip\": \"tailscale down\"}"
     return
   fi
 
@@ -18,48 +32,50 @@ tailscale_status() {
     local country_code hostname
     country_code=$(echo "$status_output" | jq -r --arg id "$exit_node_id" '.Peer[] | select(.ID == $id) | .Location.CountryCode // (.Location.Country // "unknown")')
     hostname=$(echo "$status_output" | jq -r --arg id "$exit_node_id" '.Peer[] | select(.ID == $id) | .HostName // "unknown"')
-    echo "{\"class\": \"connected\", \"text\": \"$country_code\", \"tooltip\": \"tailscale up\nexit node: $hostname ($country_code)\"}"
+    echo "{\"text\": \"󱗼$country_code\", \"class\": \"connected\", \"tooltip\": \"tailscale up\nexit node: $hostname ($country_code)\"}"
   else
-    echo "{\"class\": \"connected\", \"tooltip\": \"tailscale up\"}"
+    echo "{\"text\": \"󱗼\", \"class\": \"connected\", \"tooltip\": \"tailscale up\"}"
   fi
 }
 
-vpn_status () {
-  nmcli connection show --active | grep --quiet "$VPN_CONN_UUID"
+vpn_up() {
+	waybar_notify
 }
 
-vpn_statusjson () {
-  status=""
-  vpn_status && status="connected" || status="disconnected"
-  echo "{\"class\": \"$status\", \"tooltip\": \"'$VPN_CONN_NAME' $status\"}"
+vpn_down() {
+	waybar_notify
 }
 
-vpn_toggle () {
-  vpn_status && vpn_down || vpn_up
+vpn_status() {
+	#echo "{\"text\": \"󱄛\", \"class\": \"connected\", \"tooltip\": \"redhat vpn up\"}"
+	return
 }
 
-vpn_up () {
-  nmcli connection up "$VPN_CONN_UUID" --ask
-  pkill -RTMIN+1 waybar
-}
+main() {
+	cmd="$1"
+	shift
+	case "$cmd" in
+		up|down|status) { vpn_"$cmd" "$@"; return; } ;;
+		tailscale|tail) { tailscale_cmd "$@"; return; } ;;
+		"") ;;
+		*) echo "unknown command: '$cmd'" && exit 1 ;;
+	esac
 
-vpn_down () {
-  nmcli connection down "$VPN_CONN_UUID"
-  pkill -RTMIN+1 waybar
-}
+  local vpn_output
+  vpn_output=$(vpn_status)
+  if [[ -n "$vpn_output" ]]; then
+    echo "$vpn_output"
+    return
+  fi
 
-main () {
-  VPN_CONN_UUID=$(nmcli -g NAME,UUID,TYPE connection show | grep vpn | head -1 | cut -d':' -f2)
-  VPN_CONN_NAME=$(nmcli -g NAME,UUID,TYPE connection show | grep vpn | head -1 | cut -d':' -f1)
+  local tailscale_output
+  tailscale_output=$(tailscale_status)
+  if [[ -n "$tailscale_output" ]]; then
+    echo "$tailscale_output"
+    return
+  fi
 
-  cmd="$1"
-  case "$1" in
-    status|statusjson|up|down|toggle) vpn_"$cmd" ;;
-    tailscale_status) tailscale_status ;;
-    "") cmd="status" ;;
-    *) echo "unknown command: '$1'" && exit 1 ;;
-  esac
-
+  echo "{\"text\": \"\", \"class\": \"disconnected\", \"tooltip\": \"direct connection\"}"
 }
 
 main "$@"
