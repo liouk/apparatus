587e45f5d78b42f0b43e37c06e328d8e65c9be1b
Ilias Rinis <iliasr@gmail.com>
2026-08-10 15:16:58 +0200

    sway/waybar: sync media display and playback controls to target the same player

--- waybar/waybar/media.sh
+++ waybar/waybar/media.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-icon=""
-paused=
-for p in $(playerctl -l); do
-	case "$(playerctl -s -p $p status)" in
-		"Playing")
-			if [[ "$p" == "spotify" ]]; then
-				icon=""
-			fi
-			echo "$icon $(playerctl -s -p $p metadata --format '{{ artist }} - {{ title }}')"
-			exit 0
-			;;
-		"Paused")
-			paused=1
-			;;
-	esac
+playing=""
+paused=""
+
+for p in $(playerctl -l 2>/dev/null); do
+  pstatus=$(playerctl -p "$p" status 2>/dev/null)
+  if [[ "$pstatus" == "Playing" && -z "$playing" ]]; then
+    playing="$p"
+  elif [[ "$pstatus" == "Paused" && -z "$paused" ]]; then
+    paused="$p"
+  fi
 done
 
-if [[ -n "$paused" ]]; then
-	icon=""
-	echo "$icon $(playerctl -s -p $p metadata --format '{{ artist }} - {{ title }}')"
-	exit 0
-fi
+target="${playing:-$paused}"
+[[ -z "$target" ]] && exit 0
+
+case "$1" in
+  status)
+    icon=""
+    [[ "$target" == spotify* ]] && icon=""
+    [[ -z "$playing" ]] && icon=""
+    echo "$icon $(playerctl -p "$target" metadata --format '{{ artist }} - {{ title }}')"
+    ;;
+  *)
+    playerctl -p "$target" "$@"
+    ;;
+esac
