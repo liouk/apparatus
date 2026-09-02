47da34bd8b00e78aa7e674eb118b3f99e410155a
Ilias Rinis <iliasr@gmail.com>
2026-09-02 16:09:07 +0200

    macos: remove kitty specific stuff

--- kitty/kitty/kitty_launch.sh
+++
@@ -1,1 +0,0 @@
-/Applications/kitty.app/Contents/MacOS/kitty -d=~ -o remember_window_size=no -o initial_window_width=140c -o initial_window_height=30c --start-as=normal --single-instance --instance-group quick
